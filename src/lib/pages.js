c229d4e2c1c758eed84cf5a29033be05e146c298
Marcos Bião <dev.marcosdly@gmail.com>
2024-09-03 07:02:25 -0300

    feat: declarative definition of app pages

---
+++ src/lib/pages.js
@@ -0,0 +1,13 @@
+/** @type {import('../components/Navbar').Link[]} */
+export default [
+  { icon: 'fi-rr-limit-hand', desc: 'Alert', href: '/alert' },
+  { icon: 'fi-rr-megaphone', desc: 'Afirmations', href: '/afirmations' },
+  { icon: 'fi-rr-turntable', desc: 'Beat', href: '/beat' },
+  { icon: 'fi-rr-music-alt', desc: 'Melody', href: '/melody' },
+  { icon: 'fi-rr-tally', desc: 'Metronome', href: '/metronome' },
+  { icon: 'fi-rr-alarm-clock', desc: 'POMODORO', href: '/pomodoro' },
+  { icon: 'fi-rr-drum', desc: 'Rythm', href: '/rythm' },
+  { icon: 'fi-rr-priority-arrow', desc: 'Sequence', href: '/sequence' },
+  { icon: 'fi-rr-wave-sine', desc: 'Tone', href: '/tone' },
+  { icon: 'fi-rr-list-check', desc: 'ToDo', href: '/todo' },
+];
