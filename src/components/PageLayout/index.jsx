d627a25e18507c7434e4e7cc5f01f94cc82e3929
Marcos Bião <dev.marcosdly@gmail.com>
2024-09-04 08:34:17 -0300

    chore(PageLayout): remove colorscheme definition

--- src/components/PageLayout/index.jsx
+++ src/components/PageLayout/index.jsx
@@ -2,7 +2,7 @@ import './page-layout.scss';
 
 export default function PageLayout({ children }) {
   return (
-    <div id="page-layout-container" className="color-comforting">
+    <div id="page-layout-container">
       <div id="page-layout">{children}</div>
     </div>
   );
