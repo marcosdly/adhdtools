9ab8dc0614fd6cf2beb6f7b1bc03f6aea298ce09
Marcos Bião <dev.marcosdly@gmail.com>
2024-09-20 12:39:28 -0300

    feat: add routes; default route is Tone

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,26 @@ import PageLayout from './components/PageLayout';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import pages from './lib/pages';
+import { ErrorBoundary, LocationProvider, Router, Route } from 'preact-iso';
+import Tone from './pages/Tone';
+
+function Page() {
+  return (
+    <LocationProvider>
+      <ErrorBoundary>
+        <Router>
+          <Route default path="/tone" component={Tone} />
+        </Router>
+      </ErrorBoundary>
+    </LocationProvider>
+  );
+}
 
 function App() {
   return (
     <PageLayout>
       <Navbar links={pages} />
+      <Page />
       <Footer />
     </PageLayout>
   );
